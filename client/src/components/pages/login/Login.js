0048e5cd3f2e05b3a13e2e5489570ff474e56d35
Jennina Färm <jennina.farm@exove.fi>
2022-02-17 10:39:30 +0200

    Login: Create login form

--- client/src/components/pages/login/Login.js
+++ client/src/components/pages/login/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../atoms/button/Button';
+import ErrorMessage from '../../atoms/error-message/ErrorMessage';
+import Form from '../../atoms/form/Form';
+import Input from '../../atoms/input/Input';
+import Label from '../../atoms/label/Label';
 
 const Login = () => {
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [error, setError] = useState();
+
+  const handleSubmit = () => {
+    console.log('submit login');
+  }
+
   return (
     <div>
       <h1> Login </h1>
+      <Form submit={handleSubmit}>
+        <Label inputId='email' content='email' />
+        <Input 
+          id='email'
+          type='email'
+          placeholder='email'
+          handleValueChange={event => setEmail(event.target.value)}
+          errorMessage='Please give a valid email'
+          required
+        />
+        <Label inputId='password' content='password'/>
+        <Input 
+          id='email'
+          type='password'
+          placeholder='email'
+          handleValueChange={event => setPassword(event.target.value)}
+          errorMessage='Please give a password'
+          required
+        />
+        {error && 
+          <ErrorMessage>
+            {error}
+          </ErrorMessage>
+        }
+        <Button type='submit'> Create account </Button>
+      </Form>
     </div>
   );
 }
