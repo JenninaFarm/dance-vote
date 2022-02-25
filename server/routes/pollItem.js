fcbcfe7c6be170c74e767c07e4ac97096b7b7791
Jennina Färm <jennina.farm@exove.fi>
2022-02-25 15:54:52 +0200

    Server: Debug why not server error

--- server/routes/pollItem.js
+++ server/routes/pollItem.js
@@ -4,14 +4,14 @@ const HttpStatus = require('../HttpStatus');
 
 const route = express.Router();
 
-// route.get('/', async (req, res) => {
-//   try {
-//     const result = await crud.getPollsByOwner(req.query.id);
-//     res.status(HttpStatus.OK).json(result.rows);
-//   } catch (err) {
-//     res.send(err);
-//   }
-// });
+route.get('/', async (req, res) => {
+  try {
+    const result = await crud.findAllUsers();
+    res.status(HttpStatus.OK).json(result.rows);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 route.post('/', async (req, res) => {
   try {
