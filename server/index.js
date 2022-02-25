b6f586db72f502d3cdcb8ace1bb7613fa0668d4e
Jennina Färm <jennina.farm@exove.fi>
2022-02-25 16:37:05 +0200

    Server: Edit cors conf

--- server/index.js
+++ server/index.js
@@ -33,13 +33,7 @@ const cors = require('cors');
 // // cross origin resource sharing
 const whitelist = ['https://dance-vote.herokuapp.com/', 'https://www.yoursite.com', 'http://127.0.0.1:3500', 'http://localhost:3000'];
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: whitelist,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 200
 }
