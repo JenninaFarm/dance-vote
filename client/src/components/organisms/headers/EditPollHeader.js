3e20c7eb49ac443984ecc7339a8b0cc5a7a6bd4e
Jennina Färm <jennina.farm@exove.fi>
2022-03-21 22:09:05 +0200

    Edit-poll: Add back navigation to poll edit header

--- client/src/components/organisms/headers/EditPollHeader.js
+++ client/src/components/organisms/headers/EditPollHeader.js
@@ -1,23 +1,20 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 import {ReactComponent as Back} from "../../../images/icons/back.svg";
 import Button from '../../atoms/button/Button';
 
-
 const EditPollHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className='edit-header'>
-      <Button className='button button--icon' >
+      <Button onClick={() => navigate('/profile')} className='button button--icon' >
         <Back className='edit-header__left-icon' />
         Back
       </Button>
     </header>
   );
-}
-
-EditPollHeader.propTypes = {
-
 }
 
 export default EditPollHeader;
